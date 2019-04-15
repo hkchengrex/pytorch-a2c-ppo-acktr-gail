e5f1a0fe273ec928056b3c4ae1895eb8c6834a3c
Rex <hkchengrex@gmail.com>
2019-04-15 14:12:53 +0800

    update args

--- a2c_ppo_acktr/arguments.py
+++ a2c_ppo_acktr/arguments.py
@@ -149,6 +149,10 @@ def get_args():
         action='store_true',
         default=False,
         help='use a linear schedule on the learning rate')
+
+    parser.add_argument(
+        'expr_id',
+        help='Experiment id')
     args = parser.parse_args()
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
